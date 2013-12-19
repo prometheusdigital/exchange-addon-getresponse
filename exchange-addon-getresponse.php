d78f69b2dd1cc0b6a7b383232f9d3f437d220b52
Git User <git@petra.ithemes.com>
2013-12-19 14:59:31 -0600

    1.0.3: updated: lib/updater

--- exchange-addon-getresponse.php
+++ exchange-addon-getresponse.php
@@ -12,7 +12,7 @@
  * Plugin Name:  iThemes Exchange - GetResponse Add-on
  * Plugin URI:   http://ithemes.com/exchange/getresponse/
  * Description:  Integrates GetResponse into the iThemes Exchange plugin.
- * Version:      1.0.2
+ * Version:      1.0.3
  * Author:       Thomas Griffin
  * Author URI:   http://thomasgriffinmedia.com/
  * Text Domain:  LION
